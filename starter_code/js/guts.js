c9ed44b6d90be3877e519ab6ae3b9a3c5a7e7e99
Emmanuel Hernandez <emmanuel.h.student@lyonscommunityschool.org>
2017-04-26 14:05:11 +0000

    calculator

--- starter_code/js/guts.js
+++ starter_code/js/guts.js
@@ -2,36 +2,47 @@
 // Your functions must be called the following:
     // multiply, divide, subtract, add, power, square_root, logBase10, generateRandomNumber
 
+// Names Emmanuel.H and Kevin.M
 
 function divide(x,y) {
 	//Write your code below this line
-
+    var sum = x/y;
+    return sum;
 }
 
 function multiply(x,y) {
+    var sum = x*y;
+    return sum;
 
 }
 
 function subtract(x,y) {
+    var sum = x-y;
+    return sum;
 
 }
 
 function add(x,y) {
-
+    var sum = x+y;
+    return sum;
 }
 
 function power(x,y) {
-
+return  Math.pow(x, y);
+  
 }
 
 function square_root(x) {
+ return Math.sqrt(x);
 
 }
 
 function logBase10(x) {
-
+return Math.log10(x)
 }
 
 function generateRandomNumber() {
+
+return Math.round(Math.random()*1000000);
 
 }
